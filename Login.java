cad434e7d77882f57aa15f9760cbbead4a8e0d81
Nupurr Barskar <baraskarnupur49@gmail.com>
2026-04-24 23:14:34 +0530

    inital commit

--- Login.java
+++ Login.java
@@ -1,23 +1,24 @@
 import java.util.*;
 
 public class Login {
+  
     public static void main(String[] args) {
-        // Login logic here
-       Scanner sc= new Scanner(System.in);
-       int attempts=0;
-       String password="admin";
+        Scanner sc = new Scanner(System.in);
+        int attempts = 0;
+        String password = "admin"; // correct password
 
+        while (attempts < 3) {
+            System.out.print("Enter password: ");
+            String input = sc.nextLine();
 
-       while(attempts<3){
-        System.out.println("Enter password:");
-        String input=sc.nextLine();
-
-        if (input.equals(password)) {
-            
+            if (input.equals(password)) {
+                System.out.println("Login Successful");
+                return;
+            } else {
+                attempts++;
+            }
         }
-       }
-      
 
-        
+        System.out.println("Account Locked");
     }
 }
